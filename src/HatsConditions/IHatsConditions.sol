5c0beb53cf002bae108bb1d8bea256a6a9575bd2
spengrah <code@spengrah.xyz>
2022-08-20 15:35:36 -0500

    move hat-zodiac to its own repo

--- src/HatsConditions/IHatsConditions.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: CC0
-
-pragma solidity >=0.8.13;
-
-interface IHatsConditions {
-    function getHatStatus(uint256 _hatId) external view returns (bool);
-}
--- src/HatsOracles/IHatsOracle.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: CC0
-pragma solidity >=0.8.13;
-
-interface IHatsOracle {
-    function getWearerStatus(address _wearer, uint256 _hatId)
-        external
-        view
-        returns (bool, bool);
-}
