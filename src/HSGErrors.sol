1fc893bee5bdf789a9b3ce141d74c0dcfd4bb10f
spengrah <code@spengrah.xyz>
2023-02-07 13:32:40 -0600

    add natspec to errors

--- src/HSGErrors.sol
+++ src/HSGErrors.sol
@@ -1,49 +1,52 @@
 // SPDX-License-Identifier: CC0
 pragma solidity >=0.8.13;
 
-// Cannot disable this guard
+/// @notice Signers are not allowed to disable the HatsSignerGate guard
 error CannotDisableThisGuard(address guard);
 
-// Cannot disable protected modules
+/// @notice Signers are not allowed to disable the HatsSignerGate module
 error CannotDisableProtectedModules(address module);
 
-// Must wear the owner hat to make changes to this contract
+/// @notice Only the wearer of the owner Hat can make changes to this contract
 error NotOwnerHatWearer(address user);
 
-// Must wear the signer hat to become a signer
+/// @notice Only wearers of a valid signer hat can become signers
 error NotSignerHatWearer(address user);
 
-// Valid signers must wear the signer hat at time of execution
+/// @notice Valid signers must wear the signer hat at time of execution
 error InvalidSigners();
 
-// This contract can only be set once as a zodiac guard on `safe`
+/// @notice This contract can only be set once as a zodiac guard on `safe`
 error GuardAlreadySet();
 
-// Can't remove a signer if they're still wearing the signer hat
+/// @notice Can't remove a signer if they're still wearing the signer hat
 error StillWearsSignerHat(address signer);
 
-// This module will always be a signer on the Safe
-error NeedAtLeastTwoSigners();
-
+/// @notice Can't add more signers than designated by `maxSigners`
 error MaxSignersReached();
 
-// Target threshold must be lower than maxSigners
+/// @notice Target threshold must be lower than `maxSigners`
 error InvalidTargetThreshold();
 
-// Min threshold cannot be higher than maxSigners or targetThreshold
+/// @notice Min threshold cannot be higher than `maxSigners` or `targetThreshold`
 error InvalidMinThreshold();
 
-// Signers already owners on the safe don't need to claim
+/// @notice Signers already on the `safe` cannot claim twice
 error SignerAlreadyClaimed(address signer);
 
+/// @notice Emitted when a call to change the threshold fails
 error FailedExecChangeThreshold();
+
+/// @notice Emitted when a call to add a signer fails
 error FailedExecAddSigner();
+
+/// @notice Emitted when a call to remove a signer fails
 error FailedExecRemoveSigner();
 
-// Cannot exec tx if safeOnwerCount < minThreshold
+/// @notice Cannot exececute a tx if `safeOnwerCount` < `minThreshold`
 error BelowMinThreshold(uint256 minThreshold, uint256 safeOwnerCount);
 
-// Can only claim signer with a valid signer hat
+/// @notice Can only claim signer with a valid signer hat
 error InvalidSignerHat(uint256 hatId);
 
 /// @notice Signers are not allowed to change the threshold
